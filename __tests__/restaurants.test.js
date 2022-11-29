7296d29410384ef923d25dfcc9440ee900c8b102
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-29 09:49:02 -0800

    clean up tests file

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -8,7 +8,7 @@ describe('restaurant routes', () => {
     return setup(pool);
   });
 
-  it('GET /api/vi/restaurants should return a list of restaurants', async () => {
+  it('GET /api/v1/restaurants should return a list of restaurants', async () => {
     // GET /api/v1/restaurants
     const response = await request(app).get('/api/v1/restaurants');
     expect(response.status).toBe(200);
@@ -54,18 +54,6 @@ describe('restaurant routes', () => {
         "website": "http://www.PipsOriginal.com",
       }
     `);
-    // expect(response.body).toEqual({
-    //   id: expect.any(String),
-    //   name: expect.any(String),
-    //   cuisine: expect.any(String),
-    //   cost: expect.any(Number),
-    //   image: expect.any(String),
-    //   website: expect.any(String),
-    //   reviews: {
-    //     stars: expect.any(Number),
-    //     detail: expect.any(String),
-    //   },
-    // });
   });
 
   afterAll(() => {
